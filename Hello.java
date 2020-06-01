4bd7b2fa2fb4cd0be929174cf197cac58a3e05c1
tkdfladldi <tkdfladldI@naver.com>
2020-06-01 14:14:44 +0900

    hello()

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	hello();
+	}
+		public static void hello() {
+			System.out.println("hello~~");
+		
+
+	}
+
+}
