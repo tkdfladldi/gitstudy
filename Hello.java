1f8e4d305a8e48a16b4848a01567b92cd44f3450
tkdfladldi <tkdfladldI@naver.com>
2020-06-01 15:25:32 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -4,11 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 	hello();
+	helloName("CaCao");
+	helloName("Elsa");
 	}
 		public static void hello() {
 			System.out.println("hello~~");
 		
 
 	}
+		public static void helloName(String name){
+			System.out.println("Hello ~~");
+		}
 
 }
